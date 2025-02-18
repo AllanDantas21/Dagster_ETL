c81fca5d7f7b9ff113fdf0f08d2a95c1f4b43a32
allandantas21 <135276676+AllanDantas21@users.noreply.github.com>
2025-02-18 16:10:39 -0300

    change expectations to ExpectColumnValuesToNotBeNull

--- etl/etl/assets.py
+++ etl/etl/assets.py
@@ -1,29 +1,42 @@
-import warnings
-import great_expectations as gx
-from great_expectations.core.batch import BatchRequest
 from great_expectations.validator.validator import Validator
+from great_expectations.core.batch import BatchRequest
+from dagster import asset, MaterializeResult
+import great_expectations as gx
 import pandas as pd
+import warnings
 
-warnings.filterwarnings("ignore", category=DeprecationWarning)
-df = pd.read_csv('data/sample_data.csv')
+@asset
+def load_data():
+    warnings.filterwarnings("ignore", category=DeprecationWarning)
+    df = pd.read_csv('data/sample_data.csv')
 
-# criar um contexto do great_expectations
-context = gx.get_context()
+    # criar um contexto do great_expectations
+    context = gx.get_context()
 
-# adicionar um data source e um data asset
-data_source = context.data_sources.add_pandas("pandas")
-data_asset = data_source.add_dataframe_asset(name="pd dataframe asset")
+    # adicionar um data source e um data asset
+    data_source = context.data_sources.add_pandas("pandas")
+    data_asset = data_source.add_dataframe_asset(name="pd dataframe asset")
 
-batch_definition = data_asset.add_batch_definition_whole_dataframe("batch definion")
-batch = batch_definition.get_batch(batch_parameters={"dataframe": df})
+    batch_definition = data_asset.add_batch_definition_whole_dataframe("batch definion")
+    batch = batch_definition.get_batch(batch_parameters={"dataframe": df})
 
-# criar uma expectativa
-expectation = gx.expectations.ExpectColumnValuesToBeBetween(
-    column="id", min_value=14, max_value=21
-)
+    # criar uma expectativa
+    expectation = gx.expectations.ExpectColumnValuesToNotBeNull(
+        column="age",
+        mostly=1
+    )
 
-# validar a expectativa
-validation_result = batch.validate(expectation)
+    # validar a expectativa
+    validation_result = batch.validate(expectation)
 
-#printar os resultados
-print(validation_result.success)
+    # Retornar MaterializeResult com metadata  baseada no resultado # DAGSTER
+    return MaterializeResult(
+        metadata={
+            "validation_status": {
+                "label": "Validation Status",
+                "value": validation_result.success,
+                "type": "success" if validation_result.success else "error",
+                "unexpected_count": validation_result.result["unexpected_count"]
+            }
+        }
+    )
