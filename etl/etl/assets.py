f57e8c81484123750941919d76957dffe997723a
allandantas21 <allandantas21.ad@gmail.com>
2025-02-19 19:39:32 +0000

    att

--- etl/etl/assets.py
+++ etl/etl/assets.py
@@ -20,23 +20,43 @@ def load_data():
     batch_definition = data_asset.add_batch_definition_whole_dataframe("batch definion")
     batch = batch_definition.get_batch(batch_parameters={"dataframe": df})
 
+    collumns = df.columns.tolist()
+    table_error = {}
+
     # criar uma expectativa
-    expectation = gx.expectations.ExpectColumnValuesToNotBeNull(
-        column="age",
-        mostly=1
-    )
+    for coll_name in collumns:
+        expectation = gx.expectations.ExpectColumnValuesToNotBeNull(
+            column=coll_name,
+            mostly=1
+        )
 
-    # validar a expectativa
-    validation_result = batch.validate(expectation)
+        # validar a expectativa
+        validation_result = batch.validate(expectation)
+        if validation_result.success is False:
+            percent = validation_result.result["unexpected_percent"]
+            body_error = { "name_collumn": coll_name, "percent": f"{percent:.2f}%",
+                            "total_elements":validation_result.result["element_count"],
+                            "total_error_count":validation_result.result["unexpected_count"]}
+            if body_error["name_collumn"] not in table_error:
+                table_error[coll_name] = []
+            table_error[coll_name].append(body_error)
+    
+    if len(table_error) != 0:
+        print(f" {len(table_error)} colunas não alcançaram as espectativas de um total de {len(collumns)}")
+    else:
+        print("Deu bom")
 
-    # Retornar MaterializeResult com metadata  baseada no resultado # DAGSTER
-    return MaterializeResult(
-        metadata={
-            "validation_status": {
-                "label": "Validation Status",
-                "value": validation_result.success,
-                "type": "success" if validation_result.success else "error",
-                "unexpected_count": validation_result.result["unexpected_count"]
-            }
-        }
-    )
+    return MaterializeResult(metadata = {
+        "validation_status": table_error
+    })
+    # # Retornar MaterializeResult com metadata  baseada no resultado # DAGSTER
+    # return MaterializeResult(
+    #     metadata={
+    #         "validation_status": {
+    #             "label": "Validation Status",
+    #             "value": validation_result.success,
+    #             "type": "success" if validation_result.success else "error",
+    #             "unexpected_percent": f"{percent:.2f}%"
+    #         }
+    #     }
+    # )
